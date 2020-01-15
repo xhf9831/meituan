239e6dbce2845b04c036a19d5e6a514b60e2f3af
moban <18215615808@163.com>
2020-01-15 18:22:40 +0800

    head

--- src/components/top/Top.vue
+++ src/components/top/Top.vue
@@ -1,14 +1,51 @@
 <template>
- <div class="header">
-   <Icon type="md-pin" />
-   <span>{{list.city}}</span>
-   <span class="change">切换城市</span>
-   <span class="near">[
-    <span v-for="item in city" :key="item.id" @click="changeCity(item)">
-       {{item}}
-    </span>
-   ]</span>
+<div>
+  <div class="header">
+   <div class="left">
+    <Icon type="md-pin" size="15" />
+    <span>{{list.city}}</span>
+    <span class="changecity">切换城市</span>
+    <span class="near">[
+      <span class="n-city" v-for="item in city" :key="item.id">
+        {{item}}
+      </span>
+    ]</span>
+    <span class="login">立即登录</span>
+    <span class="register">注册</span>
+   </div>
+   <div class="right">
+     <span class="font" @mousemove="goin(1)" @mouseleave="goout">我的美团</span>
+     <span class="font">手机APP</span>
+     <span class="font" @mousemove="goin(2)" @mouseleave="goout">商家中心</span>
+     <span class="font" @mousemove="goin(3)" @mouseleave="goout">美团规则</span>
+     <span class="font">网站导航</span>
+   </div>
  </div>
+ <div>
+  <div :class="{change:num===1}" class="content">
+    <div>我的订单</div>
+    <div>我的收藏</div>
+    <div>抵用券</div>
+    <div>账户设置</div>
+  </div>
+  <div :class="{change:num===2}" class="content">
+    <div>美团餐饮商户中心</div>
+    <div>登录商家中心</div>
+    <div>美团智能收银</div>
+    <div>我想合作</div>
+    <div>手机免费开店</div>
+    <div>餐饮代理商招募</div>
+    <div>商家申请开票</div>
+    <div>免费合作美团排队</div>
+  </div>
+  <div :class="{change:num===3}" class="content">
+    <div>规则中心</div>
+    <div>规则目录</div>
+    <div>规则评议院</div>
+  </div>
+ </div>
+</div>
+ 
 </template>
 
 <script>
@@ -16,7 +53,8 @@
    data () {
      return {
        list:[],
-       city:[]
+       city:[],
+       num:0
      }
    },
    components: {
@@ -46,8 +84,13 @@
          console.log(err);
        })
      },
-     changeCity(item){
-
+     goin(item){
+       console.log(item,'111');
+       this.num = item
+     },
+     goout(){
+       console.log(item,'000');
+       this.num = 0
      }
    },
    mounted() {
@@ -69,8 +112,58 @@
 .header{
   width: 1190px;  
   margin: 0 auto;
-  .change{
-    border:1px solid black;
+  display: flex;
+  justify-content: space-between;
+  background: #F8F8F8;
+  font-size: 12px;
+  color: #999;
+  height: 40px;
+  line-height: 40px;
+  .left{
+    .changecity{
+      background: #F4F4F4;
+      border: 1px solid #E5E5E5;
+      border-radius: 2px;
+      color: #666;
+      margin: 0 4px;
+      padding: 0 2px;
+      cursor: pointer;
+      &:hover{
+        color:#FE8C00
+      }
+    }
+    .near{
+      .n-city{
+        margin: 0 4px;
+        cursor: pointer;
+        &:hover{
+          color:#FE8C00
+        }
+      }
+    }
+    .login{
+      margin-left: 25px;
+      color:#FE8C00;
+      cursor: pointer;
+    }
+    .register{
+      margin-left: 10px;
+      cursor: pointer;
+      &:hover{
+        color:#FE8C00
+      }
+    }
   }
+  .right{
+    .font{
+      padding: 12px 14px;
+    }
+  }
+}
+.content{
+  display: none;
+}
+.change{
+  display: block !important;
 }
 </style>
